8e577e5893b755d23cc79057705e13df6bf4e3a1
c0a250426f <c0a250426f@edu.teu.ac.jp>
2026-04-14 16:06:05 +0900

    練習56789

--- pygame_image.py
+++ pygame_image.py
@@ -9,24 +9,26 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
-    bg_img = pg.image.load("fig/pg_bg.jpg")
 
+    bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img,True,False)
     koukaton = pg.image.load("fig/3.png")
     koukaton = pg.transform.flip(koukaton,True,False)
 
     tmr = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+        x = tmr%3200
+        screen.blit(bg_img,[-x,0])
+        screen.blit(bg_img2,[-x+1600,0])
+        screen.blit(bg_img,[-x+3200,0])         
         screen.blit(koukaton,[300,200])
         pg.display.update()
-
-        tmr += 1        
-        clock.tick(10)
-
-
+        tmr += 1
+        clock.tick(200)
 
 if __name__ == "__main__":
     pg.init()
